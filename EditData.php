6d637a541f37413a4913027805e514c0fa50b153
Papoolob <KPtch>
2018-04-04 02:22:47 +0700

    ok

--- EditData.php
+++ EditData.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-  <title>Edit Data
+  <title>2Edit Data
   </title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
